91106308f5ff93d6e8f50c11bf0e67dc8ef3f180
Konrad Beckmann <konrad.beckmann@gmail.com>
2016-03-31 22:19:18 +0200

    Added license for draw.c

--- examples/u8g_test/draw.c
+++ examples/u8g_test/draw.c
@@ -1,8 +1,41 @@
+
+/*
+  draw.c
+  >>> PLEASE UNCOMMENT DISPLAY TYPE IN THE MAIN PROCEDURE <<<
+  ARM U8glib Example
+  Universal 8bit Graphics Library
+
+  Copyright (c) 2013, olikraus@gmail.com
+  All rights reserved.
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+  * Redistributions of source code must retain the above copyright notice, this list
+    of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "u8g/u8g.h"
 
 extern u8g_t u8g;
 
-// Just a bunch of demo draw code
+// Just a bunch of demo draw code, based on https://github.com/olikraus/u8glib/blob/master/sys/arm/examples/u8g_logo/u8g_logo.c
+// and https://github.com/olikraus/u8glib/blob/master/sys/arm/examples/graphics_test/graphics_test.c
 
 void u8g_prepare(void) {
   u8g_SetFont(&u8g, u8g_font_6x10);
